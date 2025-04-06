d23f001569cf2535c1306fadc0c01bb23eb1fa68
rahul110804 <rahul.110804@gmail.com>
2025-04-06 12:14:53 +0530

    efdppk

---
+++ src/Pages/auth/Register.jsx
@@ -0,0 +1,7 @@
+// src/Pages/auth/Register.jsx
+
+function Register() {
+    return <div>Register Page</div>;
+}
+
+export default Register; // ✅ This line is important
---
+++ src/Pages/user/EventDetails.jsx
@@ -0,0 +1,8 @@
+// src/Pages/user/EventDetails.jsx
+
+function EventDetails() {
+    return <div>Event Details Page</div>;
+  }
+  
+  export default EventDetails; // <== This is important
+  
